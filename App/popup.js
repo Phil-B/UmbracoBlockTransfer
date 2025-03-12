6c23965bed4ff595e7b516b87a30bc03c2529fb4
Phil Bacon <Phil-B@users.noreply.github.com>
2025-03-12 14:38:15 +0000

    Use extension storage instead of native clipboard - prevents invalid JSON pasted from other sources and other clipboard clashes

--- App/popup.js
+++ App/popup.js
@@ -14,55 +14,45 @@ if (copyButton && pasteButton) {
 
 function copyUmbracoClipboard() {
     const data = localStorage.getItem("ls.umbClipboardService");
+
     if (data) {
-        navigator.clipboard.writeText(data).then(() => {
-            alert("Copied Umbraco clipboard to system clipboard");
-        }).catch(err => {
-			console.error("Clipboard write failed", err);
-			alert("Clipboard write failed");
-		});
+        chrome.storage.local.set({ umbClipboard: data }, () => {
+            alert("Copied Umbraco clipboard");
+        });
     } else {
         alert("No Umbraco clipboard data found.");
     }
 }
 
 function pasteUmbracoClipboard() {
-	
-    navigator.clipboard.readText().then(text => {
-        if (text) {
-			
-            let existingData = localStorage.getItem("ls.umbClipboardService");
-            let parsedExisting = existingData ? JSON.parse(JSON.parse(existingData)) : {};
-			
-			var entries = parsedExisting.entries ?? [];
-			
-			try {
-				let pastedData = JSON.parse(JSON.parse(text));
-				let pastedEntries = pastedData.entries ?? [];
-				
-				pastedEntries.forEach((entry, index) => {
-					pastedEntries[index].label = `${entry.label} (IMPORTED)`;
-				});
-				entries = entries.concat(pastedEntries);
-				
-			} catch (e) {
-				alert("Invalid clipboard");
-				return;
-			}
-			
-			var newObject = { entries: entries };
-			
-			let newString = JSON.stringify(JSON.stringify(newObject));
-			
-			localStorage.setItem("ls.umbClipboardService", newString);
-			alert("Pasted Umbraco clipboard content");
-			
-			
+    chrome.storage.local.get("umbClipboard", (result) => {
+        let existingData = localStorage.getItem("ls.umbClipboardService");
+        let parsedExisting = existingData ? JSON.parse(JSON.parse(existingData)) : {};
+
+        var entries = parsedExisting.entries ?? [];
+
+        if (result.umbClipboard) {
+            try {
+                let pastedData = JSON.parse(JSON.parse(result.umbClipboard));
+                let pastedEntries = pastedData.entries ?? [];
+
+                pastedEntries.forEach((entry, index) => {
+                    pastedEntries[index].label = `${entry.label} (IMPORTED)`;
+                });
+
+                entries = entries.concat(pastedEntries);
+            } catch (e) {
+                alert("Invalid clipboard data");
+                return;
+            }
+
+            var newObject = { entries: entries };
+            let newString = JSON.stringify(JSON.stringify(newObject));
+
+            localStorage.setItem("ls.umbClipboardService", newString);
+            alert("Pasted Umbraco clipboard");
         } else {
-			alert("Empty/Invalid clipboard");
-		}
-    }).catch(err => {
-		alert("Clipboard read failed");
-		console.error("Clipboard read failed", err);
-	});
+            alert("No clipboard data found");
+        }
+    });
 }
